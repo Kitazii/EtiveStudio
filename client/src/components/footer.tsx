900e7f155ec0defd4615b34c4ce83148e1a15a43
Kieran Burns <101227969+Kitazii@users.noreply.github.com>
2025-07-07 16:06:10 +0000

    margin btm on mobile so that contact- overlay is in view

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
 
   return (
     <footer className="bg-white text-brand-black py-12 border-t border-gray-200">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-16">
         <div className="flex flex-col md:flex-row justify-between items-center">
           {/* Logo and Copyright */}
           <div className="mb-6 md:mb-0 ">
